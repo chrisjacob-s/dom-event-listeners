3662052a1e060857b605cea6896fa2e6c7c5868d
chrisjacob-s <83893959+chrisjacob-s@users.noreply.github.com>
2021-12-03 17:23:49 -0800

    Updated expandArticleBody and highlightArticle
    Functions are now working.
    "- Test -" are solutions that did not work.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 // import "./styles.css";
 
 /*
-In this function first select all the buttons with the class .expand_button. On each button add a click event listener that does the following:
+In this function first select all the buttons with the class .expand_button. 
+On each button add a click event listener that does the following:
 
 Find the article in which the button that was clicked belongs.
-If the text on the button that was clicked is a 'V' then set the display property of the article's body to 'none'. Also set the text on the button to '>'.
-If the text on the button that was clicked is not a 'V' then set the display property of the article's body to 'block'. Also set the text on the button to 'V'.
-
+If the text on the button that was clicked is a 'V' 
+then set the display property of the article's body to 'none'. 
+Also set the text on the button to '>'.
+If the text on the button that was clicked is not a 'V' 
+then set the display property of the article's body to 'block'. 
+Also set the text on the button to 'V'.
 Add event listeners to the .expand_button buttons
 */
 function expandArticleBody() {
@@ -16,23 +20,40 @@ function expandArticleBody() {
     btn.addEventListener("click", (event) => {
       event.preventDefault();
 
+      // Changes ">" to "V"
+      // Vise versa
       let hide = btn.textContent === "V";
       btn.textContent = hide ? ">" : "V";
-      
-      let article = document.querySelector(".article_body");
 
-      article.style.display = hide ? "none" : "block";
+      // - Test - 
+      // const article = document.querySelectorAll(".article_body");
+      // const header = document.querySelectorAll("h2");
+      // const parentButtonClick = event.target.parentNode;
+      // for (let i = 0; i < article.length; i++) {
+      //   article[i].style.display = hide ? "none" : "block";
+      // }
+
+      // Access to each btn article
+      const grand = btn.parentNode.parentNode.parentNode
+      // Access to each btn article body
+      const body = grand.querySelector(".article_body");
+
+      // Changes display from none to block
+      // Vise versa
+      body.style.display = hide ? "none" : "block";
     });
   });
 }
 
 /*
-In this function first select all buttons with the class .highlightBtn. On each button add a click event listener that does the following:
+In this function first select all buttons with the class .highlightBtn. 
+On each button add a click event listener that does the following:
 
 Find the article in which the button that was clicked belongs.
-If the text on the button is '+' then add the .highlight class to the article and set the text on the button to '-'.
-If the text on the button is not '+' then remove the .highlight class from the article and set the text on the button to '+'.
-
+If the text on the button is '+' then add the .highlight class to the article 
+and set the text on the button to '-'.
+If the text on the button is not '+' then remove the .highlight class from the article 
+and set the text on the button to '+'.
 Add event listeners to the .highlightBtn buttons
 */
 function highlightArticle() {
@@ -42,16 +63,35 @@ function highlightArticle() {
     btn.addEventListener("click", (event) => {
       event.preventDefault();
 
+      // Changes "+" to "-"
+      // Vise versa
       let hide = btn.textContent === "-";
       btn.textContent = hide ? "+" : "-";
 
-      let article = document.querySelector("article");
-      // let highlight = style.classList.add("highlight")
-      // let noHighlight = style.classList.remove("highlight")
+      // - Test -
+      // let article = document.querySelectorAll("article");
 
-      
-    })
-  })
+      // for (let i = 0; i < article.length; i++) {
+      //   if (!hide) {
+      //     article[i].classList.add("highlight");
+      //   } else {
+      //     article[i].classList.remove("highlight");
+      //   }
+      // }
+
+      // Access to each btn article 
+      const highlight = btn.parentNode.parentNode;
+
+      // If it is not "-" upon click, add class highlight
+      if (!hide) {
+        highlight.classList.add("highlight");
+      }
+      // If it is "-" upon click, remove class highlight
+      else {
+        highlight.classList.remove("highlight");
+      }
+    });
+  });
 }
 
 function main() {
@@ -60,3 +100,4 @@ function main() {
 }
 
 main();
+
